aeb9d37c741a5e43c30d16f0fe142ecb4f23a0b3
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2023-11-06 18:18:11 -0500

    adds basic pages for the front-end

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+/**
+ * Footer()
+ * =================================
+ * Footer for the webpage
+ * 
+ * @returns {ReactComponentElement}
+ */
+export default function Footer(){
+    return(
+        <footer>
+            <p>This webpage is created by Sung Yi</p>
+        </footer>)
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+export default function NavBar(){
+    return(
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+            </li>
+        </ul>
+    )
+}
