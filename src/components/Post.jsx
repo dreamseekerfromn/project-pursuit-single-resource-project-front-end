1a960a7fdab8bc74f9b3e4a33d96e47c1253f05e
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2023-11-07 22:53:17 -0500

    working for card design for single post w/ using bootstrap

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+
 function Post({ post, index }) {
   return (
-   
-      <td>
-        <Link className="td" to= {`/posts/${post.id}`}>See More...</Link>
-      </td>
+    <Link to={`/posts/${post.id}`}>
+        <div className="card">
+            <h5 className="card-header">{/* title should b here */}</h5>
+            <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <p class="card-text">{/** message should b here */}</p>
+            </div>
+        </div>
+    </Link>
    
   );
 }
