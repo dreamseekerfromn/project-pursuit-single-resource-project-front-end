8061783d39582f5a1f1fa26e7b2b060788a1e893
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2023-11-07 23:04:51 -0500

    adds fetch.js & interfaces directory; fetch.js will be used later for cleaning, interfaces for initializing obj for useState hook

---
+++ src/api/fetch.js
@@ -0,0 +1,6 @@
+export async function getAllMessages() {
+    return await fetch(`${URL}/forums`).then((res) => {
+        console.log(res);
+        return res.json();
+    });
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,19 +1,22 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { postsObjInitializer } from "../interfaces/fetchingObjInitializer";
 
 function Post({ post, index }) {
-  return (
-    <Link to={`/posts/${post.id}`}>
-        <div className="card">
-            <h5 className="card-header">{/* title should b here */}</h5>
-            <div className="card-body">
-                <h5 className="card-title">Special title treatment</h5>
-                <p class="card-text">{/** message should b here */}</p>
+    const [ singlePost, setSinglePost ] = useState(postsObjInitializer);
+    
+    return (
+        <Link to={`/posts/${post.thread_id}`}>
+            <div className="card">
+                <h5 className="card-header">{/* title should b here */}</h5>
+                <div className="card-body">
+                    <h5 className="card-title">Special title treatment</h5>
+                    <p class="card-text">{/** message should b here */}</p>
+                </div>
             </div>
-        </div>
-    </Link>
-   
-  );
+        </Link>
+
+    );
 }
 
 export default Post;
