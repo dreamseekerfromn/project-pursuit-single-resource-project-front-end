77a5231f6b1bcc4302cd20d5beca29f1152f5e80
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2023-11-08 00:10:47 -0500

    adds basic fetch functions to fetch.js

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,6 +1,44 @@
+const URL = import.meta.env.VITE_APP_API_URL;
+
 export async function getAllMessages() {
-    return await fetch(`${URL}/forums`).then((res) => {
+    return await fetch(`${URL}/posts`).then((res) => {
         console.log(res);
         return res.json();
+    });
+}
+
+// index/Get one
+export async function getSingleSong(id) {
+    return await fetch(`${URL}/posts/${id}`).then((res) => res.json());
+}
+
+// Create
+export async function createSong(item) {
+    return await fetch(`${URL}/posts/`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(item),
+    });
+}
+
+// Update
+export async function updateSong(id, item) {
+    return await fetch(`${URL}/posts/${id}`, {
+        method: "PUT",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(item),
+    });
+}
+
+// Delete
+export async function destroySong(id) {
+    return await fetch(`${URL}/posts/${id}`, {
+        method: "DELETE",
     });
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,15 +3,14 @@ import { Link } from "react-router-dom";
 import { postsObjInitializer } from "../interfaces/fetchingObjInitializer";
 
 function Post({ post, index }) {
-    const [ singlePost, setSinglePost ] = useState(postsObjInitializer);
-    
     return (
         <Link to={`/posts/${post.thread_id}`}>
             <div className="card">
                 <h5 className="card-header">{/* title should b here */}</h5>
                 <div className="card-body">
                     <h5 className="card-title">Special title treatment</h5>
-                    <p class="card-text">{/** message should b here */}</p>
+                    {post.message_pic ? (<img className="card-img-top" src={post.message_pic} />) : null}
+                    <p class="card-text">{post.thread_message}</p>
                 </div>
             </div>
         </Link>
