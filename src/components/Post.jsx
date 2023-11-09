3f87cafd893fd6f06a2deaa76edf343605ee7642
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2023-11-09 03:23:05 -0500

    adds profile_pic and time stamp for single post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,18 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { postsObjInitializer } from "../interfaces/fetchingObjInitializer";
+import "./Post.css";
 
 function Post({ post, index }) {
     return (
         <Link to={`/posts/${post.thread_id}`}>
             <div className="card">
-                <h5 className="card-header">{/* title should b here */}</h5>
+                <div className="card-header">
+                    <img className="profile_pic" src={post.profile_pic ? post.profile_pic : "src/assets/default_profile_pic.jpeg"}/>
+                    <span className="user_name">{post.user_name}</span>
+                </div>
                 <div className="card-body">
-                    <h5 className="card-title">Special title treatment</h5>
+                    <span className="time_stamp">{Date(Number(post.time_stamp) * 1000).toLocaleString()}</span>
                     {post.message_pic ? (<img className="card-img-top" src={post.message_pic} />) : null}
                     <p class="card-text">{post.thread_message}</p>
                 </div>
